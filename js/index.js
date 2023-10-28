3f6afaebff2e13eb4126d3ed3148d9903addaafe
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2023-10-28 00:43:24 -0400

    css added

--- js/index.js
+++ js/index.js
@@ -17,4 +17,28 @@ function addToCart(name, price) {
     
 
 }
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code, including the showSlides function
+    let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+});
+
 
