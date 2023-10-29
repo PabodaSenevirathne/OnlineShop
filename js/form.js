e51e4628ef11d9e8905b9c771f3314a7fdd1fcb1
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2023-10-28 23:15:10 -0400

    validation updated

--- js/form.js
+++ js/form.js
@@ -70,27 +70,11 @@ function validateForm() {
     if (isValid) {
         saveFormDetailsToLocalStorage();
         alert("Your order has been placed! Thank you for shopping with us.");
-        //    openPopupWindow();
+         // openPopupWindow();
 
     }
 
     return isValid;
-
-
-
-
-    // Add a check to compare 'Password' and 'Confirm Password'
-    if (password !== confirmPassword) {
-        displayErrorMessage("passwordValidationError", "Password and Confirm Password must match.");
-        return false;
-    }
-
-    // Rest of your validation logic...
-
-    return true;
-
-
-
 }
 
 function displayErrorMessage(id, message) {
@@ -105,15 +89,37 @@ function clearErrorMessages() {
     }
 }
 
+// function openPopupWindow() {
+//     var popupURL = "reciept.html";
+//     var width = 800;
+//     var height = 800;
+//     var left = (screen.width - width) / 2;
+//     var top = (screen.height - height) / 2;
+//     window.open(popupURL, "_blank", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top);
+
+//     displayCheckoutReceipt()
+// }
+
+
 function openPopupWindow() {
     var popupURL = "reciept.html";
     var width = 800;
     var height = 800;
     var left = (screen.width - width) / 2;
     var top = (screen.height - height) / 2;
-    window.open(popupURL, "_blank", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top);
 
-    displayCheckoutReceipt()
+    // Calculate the total purchase amount
+    const cartItems = JSON.parse(localStorage.getItem("cart"));
+    const totalPurchaseAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+
+    if (totalPurchaseAmount < 10) {
+        // Display an error message if the total purchase amount is less than $10
+        alert("The minimum purchase should be $10 or more. Please add more items to your cart.");
+    } else {
+        // Open the popup window and display the receipt
+        window.open(popupURL, "_blank", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top);
+        displayCheckoutReceipt();
+    }
 }
 
 
@@ -153,17 +159,55 @@ function displayCheckoutReceipt() {
 
     // Get the receipt elements
     const cartItemsList = document.getElementById("cartItems");
+    const subTotal = document.getElementById("subTotal")
     const cartTotal = document.getElementById("cartTotal");
 
-    // Display cart items on the receipt
-    if (cartItems && cartItems.length > 0) {
+
+    const taxAmount = document.getElementById("taxAmount")
+
+    if (!cartItems || cartItems.length === 0) {
+        // Display an error if the cart is empty
         cartItemsList.innerHTML = "";
-        cartItems.forEach(item => {
-            const listItem = document.createElement("li");
-            listItem.textContent = `${item.name} x${item.quantity} - $${(item.price * item.quantity).toFixed(2)}`;
-            cartItemsList.appendChild(listItem);
-        });
+        cartTotal.textContent = "";
+        taxAmount.textContent = "";
+        subTotal.textContent = "";
+        alert("Your cart is empty. Please add items to your cart before proceeding.");
+        return;
     }
+
+    // Calculate the subtotal and sales tax based on the province/territory
+    const subtotal = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    const provinceTaxRates = {
+        Ontario: 0.13,
+        Alberta: 0.05,     // Alberta has a 5% tax rate
+        BritishColumbia: 0.07, // British Columbia has a 7% tax rate
+        // Add tax rates for other provinces/territories here
+    };
+
+
+    const selectedProvince = checkoutDetails.province;
+    const salesTaxRate = provinceTaxRates[selectedProvince] || 0;
+    const salesTax = subtotal * salesTaxRate;
+
+    // Calculate the total cost including sales tax
+    const total = subtotal + salesTax;
+
+    // Check if the total cost is $10 or more
+    if (total < 10) {
+        cartItemsList.innerHTML = "";
+        cartTotal.textContent = "";
+        taxAmount.textContent = "";
+        subTotal.textContent = "";
+        alert("The minimum purchase should be $10 or more. Please add more items to your cart.");
+        return;
+    }
+
+    cartItemsList.innerHTML = "";
+    cartItems.forEach(item => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${item.name} x${item.quantity} - $${(item.price * item.quantity).toFixed(2)}`;
+        cartItemsList.appendChild(listItem);
+    });
 
     // Display cart total on the receipt
     if (checkoutDetails) {
@@ -177,10 +221,40 @@ function displayCheckoutReceipt() {
     }
 
     // Calculate and display the total
-    if (cartItems) {
-        const total = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2);
-        cartTotal.textContent = total;
-    }
+    // if (cartItems) {
+    //     const total = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2);
+    //     cartTotal.textContent = total;
+    // }
+
+    // Display the tax amount on the receipt
+    //         taxAmount.textContent = `$${salesTax.toFixed(2)}`;
+
+    //    // Display the subtotal, sales tax, and total on the receipt
+    //    cartTotal.textContent = `Subtotal: $${subtotal.toFixed(2)}<br>Tax (${selectedProvince}): $${salesTax.toFixed(2)}<br>Total: $${total.toFixed(2)}`;
+
+    subTotal.textContent = `${subtotal.toFixed(2)}`
+    // Display the tax amount on the receipt
+    //taxAmount.textContent = `${salesTax.toFixed(2)}`;
+
+    // taxAmount.textContent = `(${selectedProvince}): $${salesTax.toFixed(2)}`;
+    taxAmount.textContent = `${selectedProvince}: $${salesTax.toFixed(2)}`;
+
+
+
+
+
+    // Display the subtotal, sales tax, and total on the receipt
+    cartTotal.textContent = `${total.toFixed(2)}`;
+}
+
+
+function Ok() {
+    // Clear local storage
+    localStorage.removeItem("cart");
+    // localStorage.removeItem("formDetails");
+
+    // Close the window
+    window.close();
 }
 
 // Call the function when the page loads
